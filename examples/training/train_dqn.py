7b76e4571303c4e887c045d15fecd0d1340d2804
Medric Sonwa <medric49@gmail.com>
2025-01-03 22:17:03 -0500

    update

--- examples/training/train_dqn.py
+++ examples/training/train_dqn.py
@@ -2,40 +2,40 @@ from dataclasses import asdict
 
 import gymnasium as gym
 
-from envquest import arguments as args, envs, agents, trainers
+from envquest import arguments, envs, agents, trainers
 
 
 def main():
     # Training arguments
-    arguments = args.TrainingArguments(
-        agent=args.DQNAgentArguments(), logging=args.LoggingArguments(save_agent_snapshots=False)
+    args = arguments.TrainingArguments(
+        agent=arguments.DQNAgentArguments(), logging=arguments.LoggingArguments(save_agent_snapshots=False)
     )
 
     # Define environment
-    env = envs.gym.make_env(**asdict(arguments.env))
+    env = envs.gym.make_env(**asdict(args.env))
 
     # Define agent
     if isinstance(env.action_space, gym.spaces.Discrete):
         agent = agents.dqn.DiscreteQNetAgent(
-            mem_capacity=arguments.agent.mem_capacity,
-            discount=arguments.agent.discount,
-            n_steps=arguments.agent.n_steps,
-            lr=arguments.agent.lr,
-            tau=arguments.agent.tau,
-            eps_start=arguments.agent.eps_start,
-            eps_end=arguments.agent.eps_end,
-            eps_step_duration=arguments.agent.eps_step_duration,
-            eps_decay=arguments.agent.eps_decay,
+            mem_capacity=args.agent.mem_capacity,
+            discount=args.agent.discount,
+            n_steps=args.agent.n_steps,
+            lr=args.agent.lr,
+            tau=args.agent.tau,
+            eps_start=args.agent.eps_start,
+            eps_end=args.agent.eps_end,
+            eps_step_duration=args.agent.eps_step_duration,
+            eps_decay=args.agent.eps_decay,
             observation_space=env.observation_space,
             action_space=env.action_space,
         )
     else:
         raise ValueError(
-            f"'[{arguments.env.task}]' task is not a discrete gym environment. This script currently only supports discrete environments."
+            f"'[{args.env.task}]' task is not a discrete gym environment. This script currently only supports discrete environments."
         )
 
     # Define trainer
-    trainer = trainers.Trainer(env, agent, arguments)
+    trainer = trainers.Trainer(env, agent, args)
 
     # Start training
     trainer.train()
--- examples/training/train_sarsa.py
+++ examples/training/train_sarsa.py
@@ -2,39 +2,39 @@ from dataclasses import asdict
 
 import gymnasium as gym
 
-from envquest import arguments as args, envs, agents, trainers
+from envquest import arguments, envs, agents, trainers
 
 
 def main():
     # Training arguments
-    arguments = args.TrainingArguments(
-        trainer=args.TrainerArguments(num_updates=1, update_every_steps=1, num_seed_steps=0),
-        agent=args.SarsaAgentArguments(),
-        logging=args.LoggingArguments(save_agent_snapshots=False),
+    args = arguments.TrainingArguments(
+        trainer=arguments.TrainerArguments(num_updates=1, update_every_steps=1, num_seed_steps=0),
+        agent=arguments.SarsaAgentArguments(),
+        logging=arguments.LoggingArguments(save_agent_snapshots=False),
     )
 
     # Define environment
-    env = envs.gym.make_env(**asdict(arguments.env))
+    env = envs.gym.make_env(**asdict(args.env))
 
     # Define agent
     if isinstance(env.action_space, gym.spaces.Discrete):
         agent = agents.sarsa.DiscreteSarsaAgent(
-            discount=arguments.agent.discount,
-            lr=arguments.agent.lr,
-            eps_start=arguments.agent.eps_start,
-            eps_end=arguments.agent.eps_end,
-            eps_step_duration=arguments.agent.eps_step_duration,
-            eps_decay=arguments.agent.eps_decay,
+            discount=args.agent.discount,
+            lr=args.agent.lr,
+            eps_start=args.agent.eps_start,
+            eps_end=args.agent.eps_end,
+            eps_step_duration=args.agent.eps_step_duration,
+            eps_decay=args.agent.eps_decay,
             observation_space=env.observation_space,
             action_space=env.action_space,
         )
     else:
         raise ValueError(
-            f"'[{arguments.env.task}]' task is not a discrete gym environment. This script currently only supports discrete environments."
+            f"'[{args.env.task}]' task is not a discrete gym environment. This script currently only supports discrete environments."
         )
 
     # Define trainer
-    trainer = trainers.Trainer(env, agent, arguments)
+    trainer = trainers.Trainer(env, agent, args)
 
     # Start training
     trainer.train()
